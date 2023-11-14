2a81767d6fdf1c314fb973853fdcc04875278eec
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-11-14 14:50:00 +0300

    added about comment

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="nav">I'm a link!</a>
     </nav>
   );
 }
@@ -17,14 +17,20 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
